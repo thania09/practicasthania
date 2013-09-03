807d9bfc23da4c28d8606c6c1dbcf5574e00259c
thania09 <tania_villalobos_93@hotmail.com>
2013-09-03 17:13:44 -0500

    trabajo

---
+++ prac/menu restaurante.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+	int opcion;
+	printf("Bienvenido al Restaurant Spettus\n\n");
+	printf("\nEnseguida se muestra el menu:");
+	printf("\n 1)Desayunos\n 2)Almuerzos\n 3)Comidas\n 4)Cenas\n");
+	scanf ("%d",&opcion);
+	switch (opcion)
+	{
+	case 1: printf ("DESAYUNOS:\n");
+	}	
+	getch (); return 0;
+}
